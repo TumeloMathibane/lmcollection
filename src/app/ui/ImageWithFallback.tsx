d3612e0c095d1c5f3bff7cbbd14b90233ff2fabb
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-19 23:12:21 +0200

    Insert default and constant url for placeholder img

--- src/app/ui/ImageWithFallback.tsx
+++ src/app/ui/ImageWithFallback.tsx
@@ -6,19 +6,16 @@ import { useState } from "react";
 interface IImageWithFallback {
   src?: string;
   alt?: string;
-  fallbackSrc?: string;
 }
 
-export default function ImageWithFallback({
-  src,
-  alt,
-  fallbackSrc,
-}: IImageWithFallback) {
-  const [imgSrc, setImgSrc] = useState<string | undefined>(src);
+export default function ImageWithFallback({ src, alt }: IImageWithFallback) {
+  const fallbackSrc =
+    "https://placehold.jp/ffffff/595959/400x500.png?text=No%20Image&css=%7B%22border-radius%22%3A%2215px%22%2C%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%23666666)%2C%20to(%23cccccc))%22%7D";
+  const [imgSrc, setImgSrc] = useState<string | undefined>(src ?? fallbackSrc);
 
   return (
     <Image
-      src={imgSrc ?? ""}
+      src={imgSrc ?? fallbackSrc}
       alt={alt ?? ""}
       width={500}
       height={500}
