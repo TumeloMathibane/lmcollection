6c480e29db43250a6863e6496b8f7963578f1574
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-20 12:54:25 +0200

    Add fallback image src as contant

---
+++ src/constants/images.ts
@@ -0,0 +1,2 @@
+export const fallbackImage =
+  "https://placehold.jp/ffffff/595959/400x500.png?text=No%20Image&css=%7B%22border-radius%22%3A%2215px%22%2C%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%23666666)%2C%20to(%23cccccc))%22%7D";
