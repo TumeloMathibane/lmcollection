228f8c1994d3ddafffe8be438d3fea99af31d1ae
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-20 15:40:27 +0200

    Add cart types

---
+++ src/stores/types.ts
@@ -0,0 +1,26 @@
+export type CartItem = {
+  productId: string;
+  productName: string;
+  productImg: string;
+  productSize: string;
+  productQty: number;
+  productPrice: number;
+};
+
+export type CartStore = {
+  items: CartItem[];
+  addItem: (
+    id: string,
+    name: string,
+    img: string,
+    size: string,
+    qty: number,
+    price: number
+  ) => void;
+  removeItem: (id: string, size: string) => void;
+  updateItemQty: (id: string, size: string, qty: number) => void;
+  clearCart: () => void;
+  getCartTotal: () => number;
+  createdAt: number | null;
+  lastModified: number | null;
+};
