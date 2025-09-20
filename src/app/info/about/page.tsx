860db6d41587fcce37fa0b324af55dcb65d517a6
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-20 22:58:28 +0200

    Implement pages fo info route

--- src/app/info/about/page.tsx
+++ src/app/info/about/page.tsx
@@ -1,32 +1,35 @@
 export default function About() {
   return (
     <main className="min-h-[42em] md:min-h-[52em] lg:min-h-[64em] xl:min-h-[22em] space-y-3 md:space-y-5 p-2 mb-5 md:m-0 md:flex md:flex-col md:justify-center-safe md:align-middle xl:w-[70%] xl:place-self-center-safe xl:py-5">
-      <h1 className="text-4xl font-bold text-stone-900 text-center">
-        About us - LM Collection
-      </h1>
-      <div className="text-justify space-y-3 md:text-center md:space-y-5">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo ea,
-          quas explicabo beatae vitae nobis, similique laborum quos optio ex
-          porro quae? Possimus eum minus facilis in tenetur. Mollitia,
-          blanditiis! Provident iste exercitationem eligendi non error magnam
-          pariatur quo ducimus necessitatibus sequi explicabo, nostrum ad
-          numquam. Consectetur consequuntur, perferendis, dolorem ab numquam,
-          laborum libero officia magnam repudiandae error nisi minima! Voluptate
-          sint aspernatur quaerat voluptatum laboriosam eius? Nesciunt debitis
-          dignissimos autem illo reprehenderit quaerat nostrum hic ipsa qui
-          voluptates maiores maxime possimus ipsam ex deleniti animi, corporis
-          dolor! Ipsum, praesentium!
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum totam
-          minima dolorum nostrum voluptatibus animi officia repellat vero ea
-          hic? Voluptas omnis doloremque recusandae neque quam voluptate quod
-          similique minima. Minima, mollitia illo et necessitatibus quasi
-          excepturi ab. Dolore aliquam autem aliquid consectetur, amet
-          reprehenderit illo, ut alias deserunt quae rerum eligendi velit
-          explicabo doloribus ducimus saepe praesentium quia iusto.
-        </p>
+      <div>
+        <h1 className="text-4xl font-bold text-stone-900 text-center">
+          About us - LM Collection
+        </h1>
+        <div className="text-justify space-y-3 md:text-center md:space-y-5">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo ea,
+            quas explicabo beatae vitae nobis, similique laborum quos optio ex
+            porro quae? Possimus eum minus facilis in tenetur. Mollitia,
+            blanditiis! Provident iste exercitationem eligendi non error magnam
+            pariatur quo ducimus necessitatibus sequi explicabo, nostrum ad
+            numquam. Consectetur consequuntur, perferendis, dolorem ab numquam,
+            laborum libero officia magnam repudiandae error nisi minima!
+            Voluptate sint aspernatur quaerat voluptatum laboriosam eius?
+            Nesciunt debitis dignissimos autem illo reprehenderit quaerat
+            nostrum hic ipsa qui voluptates maiores maxime possimus ipsam ex
+            deleniti animi, corporis dolor! Ipsum, praesentium!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+            totam minima dolorum nostrum voluptatibus animi officia repellat
+            vero ea hic? Voluptas omnis doloremque recusandae neque quam
+            voluptate quod similique minima. Minima, mollitia illo et
+            necessitatibus quasi excepturi ab. Dolore aliquam autem aliquid
+            consectetur, amet reprehenderit illo, ut alias deserunt quae rerum
+            eligendi velit explicabo doloribus ducimus saepe praesentium quia
+            iusto.
+          </p>
+        </div>
       </div>
     </main>
   );
---
+++ src/app/info/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <main className="min-h-[42em] md:min-h-[52em] lg:min-h-[64em] xl:min-h-[22em]">
+      <p>This is the Contact page</p>
+    </main>
+  );
+}
