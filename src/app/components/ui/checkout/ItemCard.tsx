2e76dc1f2a0e46409388e17124e59f4d607b3f1b
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-23 20:38:47 +0200

    Implement item card for order summary in checkout

---
+++ src/app/components/ui/checkout/ItemCard.tsx
@@ -0,0 +1,30 @@
+import type { CartItem } from "@/stores/types";
+import ImageWithFallback from "../../ImageWithFallback";
+
+type ItemCardProp = {
+  item: CartItem;
+};
+
+export default function ItemCard({ item }: ItemCardProp) {
+  return (
+    <div className="flex space-x-4">
+      <div className="img relative">
+        <div className="size-20 overflow-hidden rounded-lg shadow-md h-fit">
+          <ImageWithFallback src={item.productImg} alt={item.productName} />
+        </div>
+        <span className="absolute -top-2 -right-2 bg-stone-800 text-stone-200 px-2 rounded-full z-100">
+          {item.productQty}
+        </span>
+      </div>
+      <div className="info w-full">
+        <div className="flex justify-between">
+          <p className="font-bold text-lg">{item.productName}</p>
+          <p className="before:content-['R'] before:mr-1">
+            {(item.productPrice * item.productQty).toFixed(2)}
+          </p>
+        </div>
+        <p className="text-stone-500">Size: {item.productSize}</p>
+      </div>
+    </div>
+  );
+}
