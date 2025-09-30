959d07b0db4b82cc14499034b6a1a2202a685ff2
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-30 23:15:27 +0200

    Implement helper functions for conversions and data generations

--- src/app/utils/helper.ts
+++ src/app/utils/helper.ts
@@ -2,18 +2,41 @@ import md5 from "md5";
 
 export function generateSignature(data: object, passphrase?: string): string {
   let procString = "";
-  Object.entries(data).map((item) => {
-    if (item[1] !== "")
-      procString += `${item[0]}=${encodeURIComponent(item[1].trim()).replace(/%20/g, "+")}&`;
+  Object.entries(data).map(([key, val]) => {
+    if (val !== "")
+      procString += `${key}=${encodeURIComponent(val).replace(/%20/g, "+")}&`;
   });
 
   procString = procString.slice(0, -1);
   if (passphrase !== null)
     procString += `&passphrase=${encodeURIComponent(passphrase ?? "").replace(/%20/g, "+")}`;
 
-  // console.log("Data: ", data);
-  // console.log("passphrase: ", passphrase);
-  // console.log("procString: ", procString);
-
   return md5(procString);
+}
+
+export function objectToString(data: object): string {
+  let output = "";
+
+  Object.entries(data).map(
+    ([key, val]) =>
+      (output += `${key}=${encodeURIComponent(val).replace(/%20/g, "+")}&`)
+  );
+
+  output = output.slice(0, -1);
+
+  return output;
+}
+
+export function generatePaymentId(
+  orderId: string,
+  passphrase?: string
+): string | void {
+  const date = new Date();
+  const encryptString = md5(date.toLocaleDateString() + orderId + passphrase);
+  let paymentId: string = "";
+  for (let i = 0; i < encryptString.length; i++) {
+    if (i % 2 !== 0) paymentId += encryptString[i];
+  }
+
+  return paymentId;
 }
