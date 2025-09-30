1e35c092d136652b632eb2505c20cf98df34194c
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-09-30 23:18:06 +0200

    Some changes...

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "placehold.jp" },
       { protocol: "https", hostname: "example.com" },
       { protocol: "https", hostname: "cdn.prod.website-files.com" },
+      { protocol: "https", hostname: "content.payfast.io" },
     ],
   },
   async redirects() {
