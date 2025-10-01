bd46625907ce6ca6f39a96ea6f403a2eeb299e4d
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-10-01 12:45:06 +0200

    Deleted files

--- src/app/info/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Policies() {
-  return (
-    <main className="min-h-[42em] md:min-h-[52em] lg:min-h-[64em] xl:min-h-[22em]">
-      <p>Info page; consists of About, Policy, & Contact page</p>
-    </main>
-  );
-}
