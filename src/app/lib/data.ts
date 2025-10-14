86bde751227d974ccbe1a64020ac4e9a886a938d
Tumelo Mathibane <tumelomathibane7@gmail.com>
2025-10-14 21:38:20 +0200

    Implement functions to provide locally served data

---
+++ src/app/lib/data.ts
@@ -0,0 +1,36 @@
+import localProducts from "@/data/new-product-catalog.json";
+import { Product } from "../(overview)/collection/products/types";
+
+// this function is for the local retrieval of products...
+export function getProducts(category?: string, count?: number): Product[] {
+  const filteredProducts: Product[] = [];
+  const countedProducts: Product[] = [];
+
+  if (category === undefined && count === undefined) return localProducts;
+
+  if (category !== undefined) {
+    localProducts.map((product) => {
+      if (product?.category === category) filteredProducts?.push(product);
+    });
+  }
+
+  if (count !== undefined) {
+    if (filteredProducts.length > 0) {
+      for (let i = 0; i < count; i++) {
+        countedProducts?.push(filteredProducts[i]);
+      }
+    } else {
+      for (let i = 0; i < count; i++) {
+        countedProducts?.push(localProducts[i]);
+      }
+    }
+  }
+
+  return countedProducts;
+}
+
+export function getProduct(id: string): Product {
+  const product = localProducts.filter((product) => product?._id === id);
+
+  return product[0];
+}
