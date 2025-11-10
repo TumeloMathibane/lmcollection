25973f4ade5172d9a3be1e93cee75901cca95172
TumeloMat <tumelomathibane7@gmail.com>
2025-11-10 20:58:11 +0200

    Revert previous commit

--- convex/schema.ts
+++ convex/schema.ts
@@ -10,14 +10,17 @@ export default defineSchema({
   product: defineTable({
     itemCode: v.string(),
     name: v.string(),
-    description: v.string(),
     price: v.number(),
-    discount: v.optional(v.number()),
-    images: v.array(v.string()),
-    additional_info: v.array(v.record(v.string(), v.string())),
+    discount: v.number(),
+    shortDescription: v.string(),
+    availableSizes: v.array(v.string()),
+    availableQuantity: v.number(),
+    category: v.string(),
+    image: v.string(),
+    additional_options: v.optional(v.record(v.string(), v.array(v.string()))),
   })
     .searchIndex("search_name", { searchField: "name" })
-    .searchIndex("search_description", { searchField: "description" }),
+    .searchIndex("search_description", { searchField: "shortDescription" }),
   orders: defineTable({
     orderNumber: v.optional(v.string()),
     items: v.array(v.record(v.string(), v.string())),
