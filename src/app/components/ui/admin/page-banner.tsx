b6201680825d793fcb7d39e8b26c717bc7752c77
TumeloMat <tumelomathibane7@gmail.com>
2025-11-12 17:18:45 +0200

    Implement banner for admin pages

--- src/app/components/ui/admin/page-banner.tsx
+++ src/app/components/ui/admin/page-banner.tsx
@@ -5,15 +5,15 @@ export default function Banner({
   searchFrom,
 }: {
   title: string;
-  searchFrom: "products" | "categories" | "customers";
+  searchFrom?: "products" | "categories" | "customers" | "all";
 }) {
   return (
     <main className="flex justify-between w-full">
-      <h1 className="capitalize">{title}</h1>
+      <h1 className="capitalize text-2xl font-bold text-stone-950">{title}</h1>
       <section>
         {searchFrom === "categories" && (
           <input
-            type="text"
+            type="search"
             className="border border-stone-300 rounded-full ps-4 py-1"
             placeholder="Search categories..."
           />
@@ -30,6 +30,14 @@ export default function Banner({
             type="text"
             className="border border-stone-300 rounded-full ps-4 py-1"
             placeholder="Search customers..."
+          />
+        )}
+
+        {searchFrom === "all" && (
+          <input
+            type="text"
+            className="border border-stone-300 rounded-full ps-4 py-1"
+            placeholder="Search Store..."
           />
         )}
       </section>
