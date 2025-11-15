0043b987a3b8b96024f9ba2604aafd255e072f38
TumeloMat <tumelomathibane7@gmail.com>
2025-11-15 20:02:43 +0000

    Some small changes

--- src/app/components/ui/admin/page-banner.tsx
+++ src/app/components/ui/admin/page-banner.tsx
@@ -1,45 +1,19 @@
 "use client";
 
 export default function Banner({
-  title,
-  searchFrom,
+  title
 }: {
   title: string;
-  searchFrom?: "products" | "categories" | "customers" | "all";
 }) {
   return (
     <main className="flex justify-between w-full">
-      <h1 className="capitalize text-2xl font-bold text-stone-950">{title}</h1>
+      <h1 className="capitalize text-3xl font-bold text-stone-950">{title}</h1>
       <section>
-        {searchFrom === "categories" && (
-          <input
-            type="search"
-            className="border border-stone-300 rounded-full ps-4 py-1"
-            placeholder="Search categories..."
-          />
-        )}
-        {searchFrom === "products" && (
-          <input
-            type="text"
-            className="border border-stone-300 rounded-full ps-4 py-1"
-            placeholder="Search products..."
-          />
-        )}
-        {searchFrom === "customers" && (
-          <input
-            type="text"
-            className="border border-stone-300 rounded-full ps-4 py-1"
-            placeholder="Search customers..."
-          />
-        )}
-
-        {searchFrom === "all" && (
-          <input
-            type="text"
-            className="border border-stone-300 rounded-full ps-4 py-1"
-            placeholder="Search Store..."
-          />
-        )}
+        <input
+          type="text"
+          className="border border-stone-400 rounded-full ps-4 py-1"
+          placeholder="Search Store..."
+        />
       </section>
     </main>
   );
