7b70fd5aafe435778dded4dda9c14c580249d51f
TumeloMat <tumelomathibane7@gmail.com>
2025-11-15 18:19:10 +0000

    Implement cover page for displaying when page is being worked on

---
+++ src/app/components/ui/under-construction.tsx
@@ -0,0 +1,5 @@
+export default function Construction() {
+    return <main className="w-full h-full absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center-safe backdrop-blur-sm">
+        <p className="text-stone-900 font-extrabold text-4xl">⚠️🚨 Under Construction 🚨⚠️</p>
+    </main>
+}
