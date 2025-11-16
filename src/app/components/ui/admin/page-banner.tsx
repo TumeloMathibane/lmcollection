43be5a15a9dcbed35b449325d611fef377d933a9
TumeloMat <tumelomathibane7@gmail.com>
2025-11-16 15:18:11 +0000

    Minor changes

--- src/app/components/ui/admin/page-banner.tsx
+++ src/app/components/ui/admin/page-banner.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-export default function Banner({
-  title
-}: {
-  title: string;
-}) {
+export default function Banner({ title }: { title: string }) {
   return (
     <main className="flex justify-between w-full">
       <h1 className="capitalize text-3xl font-bold text-stone-950">{title}</h1>
