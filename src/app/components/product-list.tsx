99bb1d9812503cca3340f4ddd25eaa90227bb374
TumeloMat <tumelomathibane7@gmail.com>
2026-01-29 18:53:02 +0000

    a

--- src/app/components/product-list.tsx
+++ src/app/components/product-list.tsx
@@ -20,8 +20,8 @@ export default function ProductList({ products }: { products: Product[] }) {
     <div className="w-full flex overflow-x-auto gap-2 md:grid md:grid-cols-3 lg:grid-cols-3 lg:gap-2">
       {products?.map(({ _id, name, price, images }) => (
         <Link key={_id} href={`/collection/products/${_id}`} className="w-full">
-          <div className="w-full h-full bg-stone-100 shadow-sm rounded-xl overflow-hidden group hover:cursor-pointer relative">
-            <figure className="overflow-hidden flex items-center w-45 h-auto sm:w-full">
+          <div className="w-45 md:w-full h-full bg-stone-100 shadow-sm rounded-xl overflow-hidden group hover:cursor-pointer relative">
+            <figure className="overflow-hidden flex items-center w-full h-auto sm:w-full">
               <div className="transition-all duration-500 group-hover:scale-105 w-full">
                 <Image
                   src={images[0] ?? ""}
@@ -33,8 +33,8 @@ export default function ProductList({ products }: { products: Product[] }) {
               </div>
             </figure>
 
-            <div className="px-3 py-1 transition-all duration-300 ease-out lg:translate-y-[150%] lg:absolute bottom-0 left-0 right-0 group-hover:translate-0 lg:bg-stone-100">
-              <h2 className="capitalize w-full text-md md:text-xl lg:text-base font-bold text-nowrap truncate">
+            <div className="w-full px-3 py-1 transition-all duration-300 ease-out lg:translate-y-[150%] lg:absolute bottom-0 left-0 right-0 group-hover:translate-0 lg:bg-stone-100">
+              <h2 className="capitalize w-full text-md font-bold text-nowrap truncate lg:text-wrap md:text-xl lg:text-base">
                 {name}
               </h2>
               <p className="before:content-['R'] before:mr-1">{price}</p>
