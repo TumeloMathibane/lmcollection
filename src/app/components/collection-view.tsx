a5a493c6b15693acc13b717a6a03ab63320dfb68
TumeloMat <tumelomathibane7@gmail.com>
2026-01-30 21:10:09 +0000

    a

--- src/app/components/collection-view.tsx
+++ src/app/components/collection-view.tsx
@@ -9,7 +9,7 @@ export default function CollectionView({ products }: { products: Product[] }) {
 
   return (
     <div
-      className={`grid grid-cols-2 gap-2 sm:grid-cols-3 lg:grid-cols-${products.length >= 4 ? 4 : products.length} `}>
+      className={`grid grid-cols-2 gap-2 sm:grid-cols-3 ${products.length >= 4 ? " lg:grid-cols-4" : `lg:grid-cols-${products.length}`}`}>
       {products.map((product, key) => {
         return (
           <div key={key}>
