7fe1a03244371cdd92b6a8148057a881a061710a
TumeloMat <tumelomathibane7@gmail.com>
2026-02-16 16:30:06 +0200

    Add admin dir. to the app/branch

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,40 @@
+import AdminSidebar from "@/components/ui/admin/sidebar-client";
+import { ConvexClientProvider } from "../ConvexClientProvider";
+import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
+import "../(overview)/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "Admin(%s) | LMCollection",
+    default: "Admin | LMCollection",
+  },
+  description: "LMCollection - Like it, Love it, & Wear it.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" data-theme="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
+        <ConvexClientProvider>
+          <AdminSidebar>{children}</AdminSidebar>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/admin/products/add-product/page.tsx
@@ -0,0 +1,17 @@
+import AddProductForm from "@/components/ui/admin/products/add-product-form";
+
+export default async function Home() {
+  return (
+    <main className="space-y-4 flex flex-col h-full p-4">
+      <section className="h-full flex flex-col gap-4">
+        <div className="flex items-center-safe justify-between">
+          <p className="text-xl font-bold text-stone-800">Add product</p>
+        </div>
+
+        <div>
+          <AddProductForm />
+        </div>
+      </section>
+    </main>
+  );
+}
