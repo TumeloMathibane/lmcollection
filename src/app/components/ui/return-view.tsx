a00ace614e7e68fdb345b5fb5a1c6a3c7ad62bea
TumeloMat <tumelomathibane7@gmail.com>
2026-02-23 01:06:45 +0200

    a

--- src/app/components/ui/return-view.tsx
+++ src/app/components/ui/return-view.tsx
@@ -17,6 +17,12 @@ export default function ReturnView() {
           process.env.VERCEL_ENV === "production" ?
             `https://${process.env.VERCEL_URL}/notify`
           : "https://d1r891fk-3000.eun1.devtunnels.ms/notify",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
         );
 
         const data = await res.json();
