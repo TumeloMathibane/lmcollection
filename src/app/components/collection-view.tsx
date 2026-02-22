718f0319a0999c24ba047c31b5043426c5f3c3e9
TumeloMat <tumelomathibane7@gmail.com>
2026-02-22 23:26:26 +0200

    Commit modifications made in relation to app icon and product collection display

--- src/app/components/collection-view.tsx
+++ src/app/components/collection-view.tsx
@@ -16,41 +16,48 @@ export default function CollectionView({ category }: { category: string }) {
 
   if (!products) return <Loading />;
 
-  if (products?.length === 0) {
-    return (
-      <>
-        <main className="flex-1 flex justify-center-safe items-center-safe">
-          <p className="text-4xl font-bold text-stone-900 text-center xl:py-2">
-            No products
-          </p>
-        </main>
-      </>
-    );
-  }
+  // if (products?.length === 0) {
+  //   return (
+  //     <>
+  //       <main className="flex-1 flex justify-center-safe items-center-safe">
+  //         <p className="text-4xl font-bold text-stone-900 text-center xl:py-2">
+  //           No products
+  //         </p>
+  //       </main>
+  //     </>
+  //   );
+  // }
 
   return (
     <div className="flex flex-col space-y-2 py-4 px-2 items-center-safe">
       <p className="text-4xl font-bold text-stone-900">Products</p>
 
-      <div className="max-w-[480px] sm:max-w-2xl lg:max-w-5xl">
+      <div className="w-full sm:max-w-2xl lg:max-w-5xl">
         <div className="space-y-4">
           <div className="border-b border-stone-300 pb-3">
             <p>
               This is the catalog page.{" "}
               {products &&
-                `(${products?.length}) ${products?.length > 1 ? "products" : "product"}`}
+                `(${products?.length}) ${products?.length > 1 || products?.length === 0 ? "products" : "product"}`}
             </p>
           </div>
 
           <div
-            className={`grid grid-cols-2 gap-2 sm:grid-cols-3 ${products.length >= 4 ? "lg:grid-cols-4" : `lg:grid-cols-${products.length}`} `}>
-            {products.map((product, key) => {
-              return (
-                <div key={key}>
-                  <ProductCard product={product} />
-                </div>
-              );
-            })}
+            className={`grid grid-cols-2 gap-2 sm:grid-cols-3 lg:grid-cols-4`}>
+            {products?.length === 0 ?
+              <div className="col-span-full">
+                <p className="text-2xl font-bold text-stone-900 text-center xl:py-2">
+                  No products
+                </p>
+              </div>
+            : products.map((product, key) => {
+                return (
+                  <div key={key}>
+                    <ProductCard product={product} />
+                  </div>
+                );
+              })
+            }
           </div>
         </div>
       </div>
