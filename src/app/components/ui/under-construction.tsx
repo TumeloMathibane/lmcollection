c41f6aa784f7bb94c1b02005227bf54ed0764074
TumeloMat <tumelomathibane7@gmail.com>
2026-03-10 11:00:03 +0200

    Revert "Change z-index for sign to be above everything"
    This reverts commit d3cc48a804b6b95e9ee0400cdf48939009e0dc33.

--- src/app/components/ui/under-construction.tsx
+++ src/app/components/ui/under-construction.tsx
@@ -1,6 +1,6 @@
 export default function Construction() {
   return (
-    <main className="fixed top-0 bottom-0 left-0 right-0 flex flex-col justify-center items-center-safe space-y-4 backdrop-blur-md backdrop-brightness-50 z-20 p-4">
+    <main className="fixed top-0 bottom-0 left-0 right-0 flex flex-col justify-center items-center-safe space-y-4 backdrop-blur-md backdrop-brightness-50 z-10 p-4">
       <div className="text-center lg:flex lg:space-x-4">
         <p className="text-white font-extrabold text-8xl lg:text-6xl">Under</p>
         <p className="text-white font-extrabold text-4xl lg:text-6xl">
