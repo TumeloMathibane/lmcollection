b9f69ab74ab1009165a3af3358ea91d88a58c6ab
TumeloMat <tumelomathibane7@gmail.com>
2026-03-10 10:58:34 +0200

    Revert "a"
    This reverts commit 6a544880fa01c722750ff2a7daa4f1a1ccee027c.

--- src/app/(overview)/page.tsx
+++ src/app/(overview)/page.tsx
@@ -130,6 +130,7 @@ export default async function Home() {
 }
 
 //! TODO  [*]: Continue with checks...
+//! TODO  [ ]: Implement product filtering and sorting functionality on the collection page to allow customers to easily find products based on their preferences (e.g. filter by price, category, popularity, etc.)...
 //! TODO  [ ]: Implement contact page logic
 //! TODO  [ ]: Admin 'add product form' - if possible, implement method to select primary image from selected images
 //! FIXME [ ]: Sort app components; components that manipulate data are to stay in components folder, and those that are only used for displaying the data must move to the subdir. of 'ui'
