1d895d8809554e5fd3eb0d472e478faa5efb3fcf
TumeloMat <tumelomathibane7@gmail.com>
2026-03-11 19:38:35 +0200

    New files in this branch

---
+++ src/app/coming-soon/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/(overview)/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | LMCollection",
+    default: "Home | LMCollection",
+  },
+  description: "LMCollection - Like it, Love it, & Wear it.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+        {children}
+      </body>
+    </html>
+  );
+}
