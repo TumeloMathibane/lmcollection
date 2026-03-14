c91ee5a58d528cd9fa73b63f64e707f0a97feb04
TumeloMat <tumelomathibane7@gmail.com>
2026-03-14 11:09:53 +0200

    Change filter to filter items by correct property and value

--- src/app/components/collection-view.tsx
+++ src/app/components/collection-view.tsx
@@ -23,11 +23,11 @@ export default function CollectionView({ category }: { category: string }) {
   ) {
     if (searchParams.get("availability") === "In stock") {
       displayProducts = displayProducts?.filter(
-        (product) => product?.availability,
+        (product) => product?.availability === "in-stock",
       );
     } else {
       displayProducts = displayProducts?.filter(
-        (product) => !product?.availability,
+        (product) => product?.availability === "out-of-stock",
       );
     }
   }
