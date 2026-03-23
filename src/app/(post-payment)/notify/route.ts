7cca0fdc514d0a77d4995fa6030c21c1a0248c29
TumeloMat <tumelomathibane7@gmail.com>
2026-03-23 14:20:15 +0200

    Remove checks from notify route

--- src/app/(post-payment)/notify/route.ts
+++ src/app/(post-payment)/notify/route.ts
@@ -1,8 +1,6 @@
-import { generatePaymentId } from "@/utils/helper";
-import md5 from "md5";
 import dns from "node:dns/promises";
 
-let paymentValidation: "PASS" | "FAIL";
+let paymentValidation: object;
 
 export async function POST(req: Request) {
   try {
@@ -12,29 +10,6 @@ export async function POST(req: Request) {
       throw new Error(`Error: ${Response.error}`);
     }
 
-    //* check no. 1: create object and string for signature verification
-    const pfObject = Object.fromEntries(
-      response
-        .split("&")
-        .map((i) =>
-          i.split("=").map((j) => decodeURIComponent(j.replace(/\+/g, " "))),
-        ),
-    );
-    let pfString = "";
-    Object.entries(pfObject).map(
-      ([key, value]) =>
-        key !== "signature" &&
-        (pfString += `${key}=${encodeURIComponent(String(value).trim()).replace(/%20/g, "+")}&`),
-    );
-    if (process.env.PAYGATE_SALT_PASSPHRASE) {
-      pfString += `passphrase=${encodeURIComponent(String(process.env.PAYGATE_SALT_PASSPHRASE).trim()).replace(/%20/g, "+")}`;
-    }
-    if (pfString.endsWith("&")) {
-      pfString = pfString.slice(0, -1);
-    }
-    const check_signature = md5(pfString) === pfObject.signature;
-
-    //* check no. 2: dns lookup for verifying request is from payfast
     let check_host = false;
     const hostnames = [
       "www.payfast.co.za",
@@ -50,35 +25,10 @@ export async function POST(req: Request) {
       }
     }
 
-    //* check no. 3: verify gross amount, by checking paymentId, to see if customer is correctly invoiced
-    const newPaymentId = generatePaymentId(
-      pfObject["item_name"],
-      Number(pfObject["amount_gross"]),
-      process.env.PAYGATE_SALT_PASSPHRASE,
-    );
-
-    const check_amount = newPaymentId === pfObject["m_payment_id"];
-
-    //* check no. 4: verify information received from gateway and confirming the order with the server before confirming the order with the client...
-    const verifyRes = await fetch(
-      "https://sandbox.payfast.co.za/eng/query/validate",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: pfString,
-      },
-    );
-    const verifyResText = await verifyRes.text();
-    const check_info = verifyResText === "VALID";
-
-    if (check_signature && check_host && check_amount && check_info) {
-      paymentValidation = "PASS";
-    } else {
-      paymentValidation = "FAIL";
+    if (!check_host) {
+      throw new Error("Error: Request not from PayFast");
     }
-
+    paymentValidation = Object.fromEntries(new URLSearchParams(response));
     return Response.json({
       status: 200,
     });
