4a89808d058cf5df467b0061e0b3133cb77d2bfa
TumeloMat <tumelomathibane7@gmail.com>
2026-04-09 15:32:03 +0200

    a

--- next.config.ts
+++ next.config.ts
@@ -13,16 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
-  async redirects() {
-    return [
-      // {
-      //   source: "/:path*",
-      //   destination: "/",
-      //   permanent: false,
-      // },
-    ];
-  },
 };
 
 export default nextConfig;
