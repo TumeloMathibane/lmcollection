93221e7d0e244c8a654e6e0a04d42525ce2fb503
TumeloMat <tumelomathibane7@gmail.com>
2026-04-09 14:10:20 +0200

    Remove unnecessary remote patterns for remote image access

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "placehold.co" },
       { protocol: "https", hostname: "placehold.jp" },
       { protocol: "https", hostname: "example.com" },
-      { protocol: "https", hostname: "cdn.prod.website-files.com" },
-      { protocol: "https", hostname: "content.payfast.io" },
-      { protocol: "http", hostname: "127.0.0.1" },
       { protocol: "https", hostname: "clean-ant-257.convex.cloud" },
       {
         protocol: "https",
