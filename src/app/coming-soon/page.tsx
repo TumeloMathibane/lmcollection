77feeccf3a1d70aa94dbf4f2216686f37ca4ac59
TumeloMat <tumelomathibane7@gmail.com>
2026-06-03 17:48:47 +0200

    Minor changes to the 'coming-soon' page

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex-1 flex flex-col bg-[url('/images/ali-pazani-3w14X-Yxffk-unsplash.jpg')] bg-cover bg-no-repeat grayscale-100 h-screen">
+    <div className="flex-1 flex flex-col bg-[url('/images/ali-pazani-3w14X-Yxffk-unsplash.jpg')] bg-cover bg-no-repeat h-screen">
       <header className="z-5">
         <div className="flex justify-center items-center-safe px-5 w-full pt-5">
           <Link href="/">
@@ -61,19 +61,19 @@ export default function Home() {
       </header>
 
       <main className="flex-1 flex">
-        <div className="absolute top-0 right-0 bottom-0 left-0 backdrop-brightness-50 backdrop-blur-[2px] z-3" />
+        <div className="absolute top-0 right-0 bottom-0 left-0 backdrop-brightness-50 backdrop-blur-[2px] z-3 grayscale-100" />
 
         <div className="h-[90vh] w-[60%] z-3 flex flex-col justify-center mx-4 md:w-[40%] md:mx-8 lg:w-full lg:h-[80vh] lg:items-center">
-          <p className="text-5xl font-extrabold text-stone-200 text-center text-wrap uppercase font-serif md:text-7xl lg:text-5xl">
+          <p className="text-5xl font-extrabold text-stone-400 text-center text-wrap uppercase font-serif md:text-7xl lg:text-5xl">
             coming soon
           </p>
 
           <div className="w-full flex items-center-safe space-x-2 lg:w-[26%]">
-            <div className="border-b border-white w-full" />
-            <p className="text-md text-stone-200 text-nowrap md:text-2xl">
+            <div className="border-b border-stone-400 w-full" />
+            <p className="text-md text-white text-nowrap md:text-2xl">
               stay tuned
             </p>
-            <div className="border-b border-white w-full" />
+            <div className="border-b border-stone-400 w-full" />
           </div>
 
           <div className="absolute bottom-0 left-0 right-0 p-5">
