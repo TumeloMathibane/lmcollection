98d14c302655f8204233825e0f4d2984fa8713ba
TumeloMat <tumelomathibane7@gmail.com>
2026-06-03 17:51:24 +0200

    a

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       </header>
 
       <main className="flex-1 flex">
-        <div className="absolute top-0 right-0 bottom-0 left-0 backdrop-brightness-50 backdrop-blur-[2px] z-3 grayscale-100" />
+        <div className="flex-1 absolute top-0 right-0 bottom-0 left-0 backdrop-brightness-50 backdrop-blur-[2px] z-3 grayscale-100" />
 
         <div className="h-[90vh] w-[60%] z-3 flex flex-col justify-center mx-4 md:w-[40%] md:mx-8 lg:w-full lg:h-[80vh] lg:items-center">
           <p className="text-5xl font-extrabold text-stone-400 text-center text-wrap uppercase font-serif md:text-7xl lg:text-5xl">
